61fecf2d2de65dce0d7afe0c0cb599a517c1502d
Mizer0211 <Sanjok60660@gmail.com>
2021-03-23 14:13:27 +0200

    01

--- 01-show-one-element/QUESTIONS.md
+++ 01-show-one-element/QUESTIONS.md
@@ -7,15 +7,17 @@
 > If you click the link to reveal more text and then refresh the page, does the text remain revealed, or is it hidden again? Why?
 
 Your reply here...
+Nē, pēc atjaunināt lapu, teksts arī būs paslēpts. Kad mēs atsvaidzinājam lapu, tā atgriežas sākotnējā stāvoklī
 
 ---
 
 > Remove `window.addEventListener("load", function(){` (and the closing `})`) from **global.js**. Does the link still reveal the text? What is the purpose of this code that you've removed?
 
 Your reply here...
-
+Ja mēs noņemsim global.js saite "more" pārstāj darboties un rādīt slēpto tekstu
 ---
 
 > Describe the the `addEventListener` method. (Remember that there is a specific, technical, methodical way to describe methods. Your reply should match that format.)
 
 Your reply here...
+AddEventListener klausas ko viņam viņam jādara, kad notika Event
--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", function(){
-
+window.onload = function() {
   // Here is some pseudo-code to help you get started:
 
   // 1. Get the DOM element which will be clicked.
+  var clickIt = document.getElementById("more_text_link");
 
   // 2. Add a listener for the 'click' event onto that element.
 
@@ -13,5 +13,17 @@ window.addEventListener("load", function(){
   //    from a hidden value to a shown value.
 
   // 5. Also modify the DOM to hide the "More info..." link.
-  
-});
+  clickIt.addEventListener("click", function(){
+
+    // 3. The block for the listener should get the DOM
+    //    element containing the text to reveal.
+    var textToShow = document.getElementById("more_text_content");
+
+    // 4. Modify that DOM element's style to change it's 'display'
+    //    from a hidden value to a shown value.  
+    textToShow.style.display = "inline"
+
+    // 5. Also modify the DOM to hide the "More info..." link.
+    clickIt.style.display = "none"
+  });
+}; 
